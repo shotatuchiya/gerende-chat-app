56e23882cacabb6f5806fa9aea5501fa5387d962
shotatuchiya <121003827+shotatuchiya@users.noreply.github.com>
2023-01-30 15:52:56 +0900

    ユーザー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 end
