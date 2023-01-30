8336fa19acaca5367331cca4dc0517f92d859a27
shotatuchiya <121003827+shotatuchiya@users.noreply.github.com>
2023-01-30 16:07:12 +0900

    room_usersテーブルの作成

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,30 @@
+class RoomsController < ApplicationController
+  def new
+    @room = Room.new
+  end
+
+  def index
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
+end
